3b96553d0873b88dc9a54f43559edb077beadbc6
TanDung <nguyentandung9111@gmail.com>
2023-02-06 03:46:34 +0700

    fix header

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,27 @@ module.exports = {
           sm: "720px",
           md: "960px",
           lg: "1140px",
-          xl: "1320px", // 1536px
+          xl: "1320px",
         },
+        
       },
     },
+    screens: {
+      'sm': '575px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 };
