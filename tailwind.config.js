23dc8a95fdde3f360162a8281348a85a0f3b7aa5
tandung <nguyentandung9111@gmail.com>
2023-02-06 13:49:37 +0700

    fix header

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,25 +11,24 @@ module.exports = {
           lg: "1140px",
           xl: "1320px",
         },
-        
       },
     },
     screens: {
-      'sm': '575px',
+      sm: "575px",
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
-    }
+    },
   },
   plugins: [],
 };
