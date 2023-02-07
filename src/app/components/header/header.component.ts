d71b89d66bdf65af981cdf9fa42a3d58777326ee
tandung <nguyentandung9111@gmail.com>
2023-02-07 17:02:21 +0700

    fix home

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
